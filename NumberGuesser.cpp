4c4d99e28ef19b2096082c891704f2551f1d1042
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-07-15 22:52:07 -0400

    Add files via upload

--- NumberGuesser.cpp
+++ NumberGuesser.cpp
@@ -6,10 +6,10 @@ using namespace std;
 /*
 Create a program called number guesser.
 There will be a secret number that is set at the start of the program without any user input.
-The secret number should be picked from a certain range. 
-The range of the number should be output to the player. 
+The secret number should be picked from a certain range.
+The range of the number should be output to the player.
 The player should be prompted to enter their guess.
-The prompt should show how many guesses the player has left. 
+The prompt should show how many guesses the player has left.
 The player will then enter their guess.
 If the guess is too high then the game will output that the guess was too high.
 If the guess is too low then the game will output that the guess was too low.
@@ -18,13 +18,19 @@ The number of guesses should be based on the range of the secret number.
 Any wrong guess will decrement the number of guess left.
 The game ends by saying what the secret number was regardless of if the player guessed the right number.
 If yes then the secret number should change.
-Any input erros by the player should have proper handling. 
-
+Any input erros by the player should have proper handling.
 */
+
+
 
 int main()
 {
-
+	 // we need a variable that stores the secret number
+	//range: 0 - 100
+	//we need a variable that stores the number of guess the player has left. 
+	// we need a variable that will store the players guess. 
+	// number of guesses will be the log base 2 of the upper range - eg. log2(100) ~ 6.64 -> 7
+	// how do we change the secret number? 
 
 	return 0;
 
