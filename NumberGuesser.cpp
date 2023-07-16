791d93b7f6a8620fc304c73ec3748334f689106b
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-07-15 22:00:43 -0400

    Add files via upload

---
+++ NumberGuesser.cpp
@@ -0,0 +1,31 @@
+//Number Guesser
+
+#include <iostream>
+using namespace std;
+
+/*
+Create a program called number guesser.
+There will be a secret number that is set at the start of the program without any user input.
+The secret number should be picked from a certain range. 
+The range of the number should be output to the player. 
+The player should be prompted to enter their guess.
+The prompt should show how many guesses the player has left. 
+The player will then enter their guess.
+If the guess is too high then the game will output that the guess was too high.
+If the guess is too low then the game will output that the guess was too low.
+The player only has a certain number of guesses.
+The number of guesses should be based on the range of the secret number.
+Any wrong guess will decrement the number of guess left.
+The game ends by saying what the secret number was regardless of if the player guessed the right number.
+If yes then the secret number should change.
+Any input erros by the player should have proper handling. 
+
+*/
+
+int main()
+{
+
+
+	return 0;
+
+}
