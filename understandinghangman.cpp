5eeebe57548ca03546013a18221bce9d84410e26
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2023-08-07 21:24:37 -0400

    Add files via upload

---
+++ understandinghangman.cpp
@@ -0,0 +1,32 @@
+// Hangman break down
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	/*
+	Create the programe Hangman. Promp the player to enter a phrase. 
+	That phrase will be hidden by a series of dashes (exluding spaces).
+	The player can then guess letters. If they guess correctly all the letters that match will be revealed.
+	If they guess incorrect, then 1 more body part will be added to the gallows.
+	If they lose the gallows should look like this:
+
+	+---+
+	|   |
+	|   o
+	|  /|\
+	|  / \
+   -+-
+
+    If the player loses, the phrase should be revealed.
+	After the game is over the player should be prompted if they want to play again.
+	*/
+
+	// we need a way of storing the secret phrase
+	// we need a way of storing the hidden phrase (with dashes)
+	// we need a variable that holds the nbumber of guesses left.  ( set to 6 initially)
+
+
+	return 0;
+}
